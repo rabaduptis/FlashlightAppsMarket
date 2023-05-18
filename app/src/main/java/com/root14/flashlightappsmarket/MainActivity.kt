de62a595e51aae89dec605bb9245268c57c63af0
root-14 <ilkaygavaz2@gmail.com>
2023-05-18 20:19:05 +0300

    feat add drawer menu functionality

--- app/src/main/java/com/root14/flashlightappsmarket/MainActivity.kt
+++ app/src/main/java/com/root14/flashlightappsmarket/MainActivity.kt
@@ -10,7 +10,13 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.lifecycle.viewModelScope
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.navigation.NavigationView
 import com.root14.flashlightappsmarket.databinding.ActivityMainBinding
+import com.root14.flashlightappsmarket.model.CategoryType
+import com.root14.flashlightappsmarket.view.ui.mainFragment.MainFragmentDirections
 import com.root14.flashlightappsmarket.viewmodel.ApplicationFragmentViewModel
 import com.root14.flashlightappsmarket.viewmodel.MainFragmentViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -29,7 +35,6 @@ class MainActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-
         //drawer layout
         drawerLayout = binding.myDrawerLayout
         actionBarDrawerToggle =
@@ -40,6 +45,44 @@ class MainActivity : AppCompatActivity() {
 
         //action bar
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        //drawer menu
+        binding.navigationView.setNavigationItemSelectedListener { item ->
+            val action = MainFragmentDirections.actionMainFragmentToApplicationFragment()
+            when (item.itemId) {
+                R.id.menu_flashlights -> {
+
+                    action.categoryType = CategoryType.FLASHLIGHTS
+                    navController.navigate(action)
+                    drawerLayout.closeDrawers()
+                    true
+                }
+
+                R.id.menu_colored_lights -> {
+                    action.categoryType = CategoryType.COLOREDLIGHTS
+                    navController.navigate(action)
+                    drawerLayout.closeDrawers()
+                    true
+                }
+
+                R.id.menu_sos_alerts -> {
+                    action.categoryType = CategoryType.SOSALERTS
+                    navController.navigate(action)
+                    drawerLayout.closeDrawers()
+                    true
+                }
+
+                else -> {
+                    //TODO: not implemented yet
+                    true
+                }
+            }
+
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
